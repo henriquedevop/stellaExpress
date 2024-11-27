fb7b7125e4e379e730915af1d9c2f9eb56377d4e
henriquedevop <wymuxgh@gmail.com>
2024-11-27 10:15:36 -0300

    feat: add readmore in producs and add more images

--- src/components/products/index.tsx
+++ src/components/products/index.tsx
@@ -1,24 +1,74 @@
-import { InboxIcon } from "@heroicons/react/16/solid";
+import { InboxIcon, ArrowDownIcon, ArrowLeftIcon } from "@heroicons/react/16/solid";
+import { useState } from "react";
 
 export function Products() {
+
+    const products = [
+        { id: "cerveja", name: "Cerveja", details: ["Heineken", "Stella", "Devassa", "Brahma"] },
+        { id: "refrigerante", name: "Refrigerantes", details: ["Coca-Cola", "Guaraná", "Pepsi"] },
+        { id: "agua", name: "Água", details: ["Água mineral", "Água 20L"] },
+        { id: "carvao", name: "Carvão", details: ["Carvão para churrasco"] }, 
+        { id: "gelo", name: "Gelo", details: ["Gelo até 20Kg", "Gelo de coco"] }, 
+        { id: "bebidasGeral", name: "Bebidas em geral", details: ["Whisky", "Gin", "Vinhos"] },
+        { id: "tabacaria", name: "Tabacaria", details: ["Tabacos", "Sedas", "Charuto", "Isqueiro"] },
+        { id: "embalagens", name: "Embalagens", details: ["Sacos plásticos", "Caixas de papelão", "Caixa de pizza", "Barca"] }, 
+        { id: "festa", name: "Artigos de festa", details: ["Pratos", "Copos", "Talheres", "Guarda-napo"] }, 
+    ];
+
+    const [expendedItems, setExpandedItems] = useState<Record<string, boolean>>({})
+
+    function toggleReadMore(id: string) {
+        setExpandedItems((prev) => ({
+            ...prev,
+            [id]: !prev[id],
+        }))
+        console.log(expendedItems)
+    }
+
     return (
         <section className="mx-8 mt-8 md:mx-16 lg:mx-24" id="products">
             <h3 className="font-extrabold text-3xl flex items-center gap-1">
                 Nossos Produtos <InboxIcon className="size-8" />
             </h3>
-            <div className="bg-white rounded-lg w-full shadow-md mt-8">
+            <div className="bg-white rounded-lg w-full shadow-md mt-8 flex flex-col justify-center">
                 <ul className="font-semibold text-2xl text-neutral-400 p-4 flex flex-col gap-2">
-                    <li>- Cerveja</li>
-                    <li>- Refrigerante</li>
-                    <li>- Água (garrafão)</li>
-                    <li>- Carvão</li>
-                    <li>- Gelo</li>
-                    <li>- Bebidas em geral</li>
-                    <li>- Aluguel de caixas térmicas</li>
-                    <li>- Tabacaria</li>
-                    <li>- Embalagens</li>
-                    <li>- Artigos para festa</li>
+                    {products.map((item) => (
+                        <li key={item.id}>
+                            <div className="flex justify-between">
+                               - {item.name}
+                               <button onClick={ () => toggleReadMore(item.id)}>{expendedItems[item.id] ? <ArrowLeftIcon className="size-6"/>
+                                : <ArrowDownIcon className="size-6"/> }</button>
+                            </div>
+                            {expendedItems[item.id] && (
+                                <ul>
+                                    {item.details.map((detail, index) => (
+                                        <li className="text-base my-1 text-gray-400 border-b mr-auto" key={index}>{detail}</li>
+                                    ))}
+                                </ul>
+                            )}
+                        </li>
+                    ))}
                 </ul>
+                <span className="text-gray-400 text-sm mb-3 w-full text-center font-semibold">Tudo isso e muito mais</span>
+                <span className="text-gray-300 text-xs mb-3 w-full text-center font-semibold">Sempre consulte nossos vendedores para saber da disponibilidade</span>
+            </div>
+            <div className="w-full h-20 mt-5 bg-black rounded-md">
+                <div className="bg-pratileira-bg w-full h-20 mt-5 bg-cover bg-center opacity-50 rounded-md"></div>
+            </div>
+            <div className="w-full h-20 mt-5 bg-black rounded-md">
+                <div className="bg-wine-bg w-full h-20 mt-5 bg-cover bg-center opacity-50 rounded-md"></div>
+            </div>
+            <div className="w-full h-20 mt-5 bg-black rounded-md">
+                <div className="bg-embalagem-bg w-full h-20 mt-5 bg-cover bg-center opacity-50 rounded-md"></div>
+            </div>
+            <div className="w-full h-20 mt-5 bg-black rounded-md">
+                <div className="bg-tabacaria-bg w-full h-20 mt-5 bg-cover bg-center opacity-50 rounded-md"></div>
+            </div>
+            <div className="w-full h-20 mt-5 bg-black rounded-md">
+                <div className="bg-snacks-bg w-full h-20 mt-5 bg-cover bg-center opacity-50 rounded-md"></div>
+            </div>
+            <div className="w-full h-20 mt-5 bg-black rounded-md">
+                <div className="bg-bebidas-bg w-full h-20 mt-5 bg-cover bg-center opacity-50 rounded-md"></div>
             </div>
         </section>
     );
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,14 @@ export default {
     extend: {
       fontFamily: {
         inter: ['Inter', 'sans-serif']
+      },
+      backgroundImage: {
+        'wine-bg': "url('/src/assets/winebg.jpeg')",
+        'pratileira-bg': "url('/src/assets/pratileirabg.jpeg')",
+        'embalagem-bg': "url('/src/assets/embalegem-bg.jpeg')",   
+        'tabacaria-bg': "url('/src/assets/tabacaria-bg.jpeg')",   
+        'snacks-bg': "url('/src/assets/snacks-bg.jpeg')",
+        'bebidas-bg': "url('/src/assets/bebidas-bg.jpg')",
       }
     },
   },
